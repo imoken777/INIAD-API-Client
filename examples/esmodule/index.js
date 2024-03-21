3d5a52dd4dc9fb658ffe436219593819ef1b8577
imoken777 <s1f102301640@iniad.org>
2024-03-21 21:22:25 +0900

    仮examples追加

---
+++ examples/esmodule/index.js
@@ -0,0 +1,16 @@
+import * as iniad from 'iniad-iot-client';
+
+const baseUrl = process.env.BASE_URL;
+const userId = process.env.USER_ID;
+const password = process.env.PASSWORD;
+
+const iotClient = new iniad.eduIotApiClient({ baseUrl, userId, password });
+
+(async () => {
+  try {
+    const res = await iotClient.getLockerInfo();
+    console.log(res.lockerAddress);
+  } catch (e) {
+    console.error(e);
+  }
+})();
