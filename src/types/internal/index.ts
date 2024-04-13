da2db5fc2f8d1b00a3273b53975facccd7aad2f7
imoken777 <s1f102301640@iniad.org>
2024-04-13 20:56:02 +0900

    型定義を公開未公開で分割

---
+++ src/types/internal/index.ts
@@ -0,0 +1,31 @@
+export type ValidatedCardIDm = string & { readonly __brand: unique symbol };
+
+export type LockerApiResponse = {
+  readonly name?: string;
+  readonly floor?: number;
+};
+
+export type ICCardInfoApiResponse = {
+  readonly id: number;
+  readonly uid: string;
+  readonly comment: string;
+};
+export type AllICCardApiResponse = Array<ICCardInfoApiResponse>;
+
+export type RoomApiResponse = Array<{
+  readonly room_num: number;
+  readonly sensor_type: string;
+  readonly value: number | null;
+}>;
+
+export type CardSignageLinkApiResponse = {
+  readonly idm: string;
+  readonly url: string;
+  readonly display_seconds: number;
+};
+export type AllCardSignageLinksApiResponse = Array<CardSignageLinkApiResponse>;
+
+export type DeleteCardSignageLinkApiResponse = {
+  readonly message: string;
+  readonly removed_count: number;
+};
---
+++ src/types/public/index.ts
@@ -0,0 +1,51 @@
+export interface StatusInfo {
+  readonly status: 'success' | 'error' | 'dummy';
+  readonly description: string;
+}
+
+export type LockerInfo = StatusInfo & {
+  readonly lockerAddress: string | null;
+  readonly lockerFloor: number | null;
+};
+
+export type ICCardInfo<IncludeStatusInfo = true> = IncludeStatusInfo extends true
+  ? StatusInfo & {
+      readonly cardIDm: string | null;
+      readonly icCardComment: string | null;
+    }
+  : {
+      readonly cardIDm: string | null;
+      readonly icCardComment: string | null;
+    };
+
+export type AllICCardInfo = StatusInfo & {
+  readonly cards: ICCardInfo<false>[];
+};
+
+export type RoomStatus = StatusInfo & {
+  readonly temperature: number | null;
+  readonly humidity: number | null;
+  readonly illuminance: number | null;
+  readonly airPressure: number | null;
+};
+
+export type CardSignageLink<IncludeStatusInfo = true> = IncludeStatusInfo extends true
+  ? StatusInfo & {
+      readonly cardIDm: string;
+      readonly url: string;
+      readonly displaySeconds: number;
+    }
+  : {
+      readonly cardIDm: string;
+      readonly url: string;
+      readonly displaySeconds: number;
+    };
+
+export type AllCardSignageLinks = StatusInfo & {
+  readonly links: CardSignageLink<false>[];
+};
+
+export type DeleteCardSignageLink = StatusInfo & {
+  readonly cardIDm: string;
+  readonly removeCount: number;
+};
